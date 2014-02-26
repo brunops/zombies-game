a485ec0ad859493b1233d171718ad2b3f8b43d5d
Bruno Sanches <brunopsanches@gmail.com>
2014-02-25 23:29:25 -0800

    Define Game object
    * Game creates a canvas object and append it to the DOM where stuff come to life
    * Has one player
    * Has a background
    * #render knows how to draw scene (player on top of background) into its canvas
    * #bind keeps track of pressed keys and move player around
    * #update updates player position according to current pressed keys (player moves yay)
    * #update recieves a modifier ensures player moves the same speed no matter how slow the script is running (nice trick!) - more info at http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/

---
+++ Game.js
@@ -0,0 +1,82 @@
+/* global Player */
+
+(function () {
+  'use strict';
+
+  var Game = {
+    init: function () {
+      Game.keysDown = {};
+
+      Game.createCanvas();
+      Game.loadBackground();
+
+      Game.player = new Player(50, 50);
+
+      Game.bind();
+    },
+
+    bind: function () {
+      document.addEventListener('keydown', function (e) {
+        Game.keysDown[e.keyCode] = true;
+      }, false);
+
+      document.addEventListener('keyup', function (e) {
+        delete Game.keysDown[e.keyCode];
+      }, false);
+    },
+
+    createCanvas: function () {
+      Game.canvas = document.createElement('canvas');
+      Game.context = Game.canvas.getContext('2d');
+
+      Game.canvas.width = 512;
+      Game.canvas.height = 480;
+
+      document.body.appendChild(Game.canvas);
+    },
+
+    loadBackground: function () {
+      Game.backgroundReady = false;
+
+      Game.backgroundImg = new Image();
+      Game.backgroundImg.src = 'public/images/background.png';
+
+      Game.backgroundImg.onload = function () {
+        Game.backgroundReady = true;
+      };
+    },
+
+    update: function (modifier) {
+      // UP
+      if (Game.keysDown[38]) {
+        Game.player.setY(Game.player.y - (Game.player.speed * modifier));
+      }
+
+      // DOWN
+      if (Game.keysDown[40]) {
+        Game.player.setY(Game.player.y + (Game.player.speed * modifier));
+      }
+
+      // RIGHT
+      if (Game.keysDown[39]) {
+        Game.player.setX(Game.player.x + (Game.player.speed * modifier));
+      }
+
+      // LEFT
+      if (Game.keysDown[37]) {
+        Game.player.setX(Game.player.x - (Game.player.speed * modifier));
+      }
+    },
+
+    render: function () {
+      if (Game.backgroundReady) {
+        Game.context.drawImage(Game.backgroundImg, 0, 0);
+      }
+
+      Game.player.render(Game.context);
+    }
+  };
+
+  // expose Game globally
+  window.Game = Game;
+}());
