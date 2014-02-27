7824449c672e1bcb086df80381975b1b3ee8672d
Bruno Sanches <brunopsanches@gmail.com>
2014-02-26 18:42:02 -0800

    Use requestAnimationFrame instead of setInterval
    * More information about it here http://paulirish.com/2011/requestanimationframe-for-smart-animating/

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 /* global Game */
+window.requestAnimFrame = (function () {
+  'use strict';
+  return window.requestAnimationFrame       ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame    ||
+         function (callback) {
+           window.setTimeout(callback, 1000 / 60);
+         };
+})();
+
 (function () {
   'use strict';
 
   Game.init();
 
-  var then = Date.now();
-  setInterval(function() {
+  var before = Date.now();
+  window.requestAnimFrame(function update() {
     var now = Date.now(),
-        delta = now - then;
+        delta = now - before;
 
     Game.update(delta / 1000);
     Game.render();
 
-    then = now;
-  }, 1000 / 60);
+    before = now;
+    window.requestAnimFrame(update);
+  });
 }());
