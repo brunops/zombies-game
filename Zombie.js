bd7715fc194f419bf42e29b557e5e0e56c2335af
Bruno Sanches <brunopsanches@gmail.com>
2014-02-26 21:52:52 -0800

    Define Zombie object

---
+++ Zombie.js
@@ -0,0 +1,27 @@
+/* global Entity */
+(function() {
+  'use strict';
+
+  function Zombie(x, y, speed) {
+    this.base = Entity;
+    this.base(
+      x,
+      y,
+      Zombie.width,
+      Zombie.height,
+      'public/images/sprites/zombies.png',
+      0,
+      32,
+      speed || 70
+    );
+  }
+
+  Zombie.width = 32;
+  Zombie.height = 32;
+
+  Zombie.prototype = new Entity();
+
+  // Make Zombie available globally
+  window.Zombie = Zombie;
+
+}());
