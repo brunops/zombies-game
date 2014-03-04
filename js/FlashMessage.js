ccd148ae27b47b0c05b8c1cb210b385f911c9bef
Bruno Sanches <brunopsanches@gmail.com>
2014-03-04 01:01:52 -0800

    Invert font and size parameters

--- js/FlashMessage.js
+++ js/FlashMessage.js
@@ -1,17 +1,17 @@
 (function () {
   'use strict';
 
-  function FlashMessage(message, x, y, color, font, size) {
-    this.init(message, x, y, color, font, size);
+  function FlashMessage(message, x, y, color, size, font) {
+    this.init(message, x, y, color, size, font);
   }
 
-  FlashMessage.prototype.init = function (message, x, y, color, font, size) {
+  FlashMessage.prototype.init = function (message, x, y, color, size, font) {
     this.message = message;
     this.x = x;
     this.y = y;
     this.color = color || '#fff';
-    this.font = font || 'Helvetica';
     this.size = size || '14px';
+    this.font = font || 'Helvetica';
 
     this.createdAt = Date.now();
   };
