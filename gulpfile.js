f699be7b35746309c46dea4b8466860950fc21b3
Bruno Sanches <brunopsanches@gmail.com>
2014-04-12 18:50:17 -0700

    Add gulpfile to watch js, css and html file changes and browserify results in build folder

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('scripts', function () {
+  return browserify('./js/main.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('./css/**/*.css')
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('./*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('./public/images/**/*')
+    .pipe(gulp.dest('./build/public/images'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./js', ['scripts']);
+  gulp.watch('./css', ['css']);
+  gulp.watch('./*.html', ['html']);
+});
+
+gulp.task('default', ['scripts', 'css', 'html', 'images', 'watch']);
